caa0850890606ee38849aa3336b9a806f84b5fcc
Jay Williams <jay@d3done.com>
2010-01-04 00:30:06 -0600

    Basic file scanner system

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -13,14 +13,84 @@ else
 	error_reporting(0); // Production
 	
 define('APPPATH', realpath(dirname(__FILE__)).'/');
+define('CACHEPATH', realpath(dirname(__FILE__).'/../cache').'/');
+define('CONTENTPATH', realpath(dirname(__FILE__).'/../content').'/');
 
+// var_dump(APPPATH,CACHEPATH,CONTENTPATH);
 
 require_once 'classes/class_uri.php';
 
 Uri::getInstance();
 
 // Uri::$clean_url=false;
-var_dump(Uri::create());
+// var_dump(Uri::create());
 
-var_dump(Uri::$parameters);
-// var_dump(Uri::$uri);
+// var_dump(Uri::$parameters);
+
+/**
+ * File Scanner
+ */
+$scan = array();
+
+// Controller Directories to Scan:
+$scan['controller'][] = APPPATH.'controllers/';
+
+// Class Directories to Scan:
+$scan['class'][] = APPPATH.'classes/';
+
+// Language Directories to Scan:
+$scan['language'][] = APPPATH.'languages/';
+
+// Page Directories to Scan:
+$scan['page'][] = CONTENTPATH.'templates/simple/';
+
+$files = array();
+foreach ($scan as $section => $dirs) {
+	foreach ($dirs as $dir) {
+ 		$matches = glob($dir.$section.'*.php');
+		foreach ($matches as $file) {
+			$files[$section][basename($file,'.php')] = $file;
+		}
+	}
+}
+
+
+var_dump($files);
+
+
+/**
+ * 
+ * @todo also scan plugin folders
+ */
+function find($type=null)
+{
+	global $files;
+	
+	if (!array_key_exists($type,$files)) 
+		return false;
+	
+	$keys = array_keys(Uri::get());
+	array_unshift($keys,$type);
+	$total = count($keys);
+
+	for ($i=0; $i < $total; $i++) {
+		$name = implode($keys,'_');
+		if (array_key_exists($name,$files[$type])) {
+			return $files[$type][$name];
+		}
+		array_pop($keys);
+	}
+}
+
+$controller = find('controller');
+
+include $controller;
+
+var_dump($controller);
+
+$page = find('page');
+
+include $page;
+
+var_dump($page);
+
---
+++ application/controllers/controller_post_category.php
@@ -0,0 +1,3 @@
+<?php
+
+echo 'Controller Post Category!';
