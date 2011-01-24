c6fe1c62fc28b1c35c5ef326497c5036fc67413d
Jay Williams <jay@d3done.com>
2011-01-24 11:25:24 -0600

    Small error message tweaks

--- plugins/error/template/error.php
+++ plugins/error/template/error.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 	<head>
-		<title>Oups !</title>
+		<title>Oops!</title>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
 	</head>
 	<body>
-		<h1>Oups !</h1>
+		<h1>Oops!</h1>
 		<p>
-			There is an internal error ! Active the debug mode in your
-			configuration, if you want more information about it !
+			It looks like we're experiencing a few problems with our website.
+			If you are the site owner, please enable debug mode in your 
+			configuration to learn more about the problem.
 		</p>
 		<p>
-			We are soory about this behaviour and we'll try to recover it soonly.
+			Otherwise, please hang in there, we'll be up and running as soon as possible.
 		</p>
 		<p>
 			<em>The Pixelpost team.</em>
--- plugins/error/template/error_debug.php
+++ plugins/error/template/error_debug.php
@@ -1,17 +1,16 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 	<head>
-		<title>Oups !</title>
+		<title>Oops!</title>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
 	</head>
 	<body>
-		<h1>Oups !</h1>
+		<h1>Oops!</h1>
 		<p>
-			There is an internal error ! Active the debug mode in your
-			configuration, if you want more information about it !
+			It looks like we're experiencing a few problems with our website.
 		</p>
 		<p>
-			We are soory about this behaviour and we'll try to recover it soonly.
+			Please hang in there, we'll be up and running as soon as possible.
 		</p>
 		<p>
 			<em>The Pixelpost team.</em>
@@ -20,7 +19,7 @@
 		<p>
 			<?php echo $error ?>
 		</p>
-		<h1>BackTrace:</h1>
+		<h1>Backtrace:</h1>
 		<p>
 			<?php echo nl2br($error->getTraceAsString()); ?>
 		</p>
