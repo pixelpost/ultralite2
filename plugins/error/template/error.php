b6d0d65f91fe8017a8ba2c1fb2167cb0f40e0ff9
Alban LEROUX <seza@paradoxal.org>
2011-01-24 03:10:05 +0100

    Add forgotten utf8 encoding in html file

--- plugins/error/template/error.php
+++ plugins/error/template/error.php
@@ -2,6 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 	<head>
 		<title>Oups !</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
 	</head>
 	<body>
 		<h1>Oups !</h1>
