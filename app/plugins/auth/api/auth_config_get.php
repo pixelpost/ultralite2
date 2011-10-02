c29bb361ca867918023c2701dc3b27c7fe71149d
Alban LEROUX <seza@paradoxal.org>
2011-10-03 00:11:41 +0200

    Plugin auth: Fix grant access check

--- app/plugins/auth/api/auth_config_get.php
+++ app/plugins/auth/api/auth_config_get.php
@@ -5,7 +5,7 @@ namespace pixelpost\plugins\auth;
 use pixelpost;
 use pixelpost\plugins\api\Exception as ApiException;
 
-if (!self::is_granted('read'))
+if (!Plugin::is_granted('read'))
 {
 	throw new ApiException('unauthorized', 'You have not the rights necessary to call this method.');
 }
--- app/plugins/auth/api/auth_config_set.php
+++ app/plugins/auth/api/auth_config_set.php
@@ -5,7 +5,7 @@ namespace pixelpost\plugins\auth;
 use pixelpost;
 use pixelpost\plugins\api\Exception as ApiException;
 
-if (!self::is_granted('config'))
+if (!Plugin::is_granted('config'))
 {
 	throw new ApiException('unauthorized', 'You have not the rights necessary to call this method.');
 }
