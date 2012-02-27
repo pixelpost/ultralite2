785789ca92b0b3e88c1f517c40e9bef8e7ee39f1
Alban LEROUX <seza@paradoxal.org>
2012-02-27 10:48:02 +0100

    Core Template.php: Remove trailing spaces

--- app/core/Template.php
+++ app/core/Template.php
@@ -31,11 +31,11 @@ class Template
 	 * @var String The template cache path
 	 */
 	protected $_tplCache = '';
-	
+
 	/**
 	 * Create a new Template
-	 * 
-	 * @return Template 
+	 *
+	 * @return Template
 	 */
 	public static function create()
 	{
@@ -47,51 +47,51 @@ class Template
 
 	/**
 	 * Change the cache setting, by default set it to true
-	 * 
+	 *
 	 * @param bool $active
-	 * @return Template 
+	 * @return Template
 	 */
     public function set_cache_raw_template($active = true)
     {
 		Filter::assume_bool($active);
-		
+
         $this->_cacheRawTemplate = $active;
-		
+
         return $this;
     }
 
 	/**
 	 * Change the template path
-	 * 
-	 * @param string $path 
+	 *
+	 * @param string $path
 	 */
 	public function set_template_path($path)
 	{
 		Filter::assume_string($var);
-		
+
 		$this->_tplPath = $path . ((substr($path, -1) == SEP) ? '' : SEP);
-		
+
 		return $this;
 	}
-	
+
 	/**
 	 * Change the cache template path
-	 * 
-	 * @param string $path 
+	 *
+	 * @param string $path
 	 */
 	public function set_template_cache_path($path)
 	{
 		Filter::assume_string($var);
-		
+
 		$this->_tplCache = $path . ((substr($path, -1) == SEP) ? '' : SEP);
-		
+
 		return $this;
 	}
-	
+
 	/**
 	 * Return the App configuration object (pixelpost\config::create())
 	 * This is a facility method to be called into template file.
-	 * 
+	 *
 	 * @return Config
 	 */
 	public function config()
@@ -101,17 +101,17 @@ class Template
 
 	/**
 	 * Add data into this template
-	 * 
+	 *
 	 * @param mixed $var
 	 * @param mixed $value
-	 * @return Template 
+	 * @return Template
 	 */
     public function assign($var, $value = null)
     {
         if (is_string($var))     $this->$var = Filter::array_to_arrayObject($value);
         elseif (!is_array($var)) throw Error::create(11);
         else                     foreach ($var as $key => $val) $this->$key = Filter::array_to_arrayObject($val);
-		
+
         return $this;
     }
 
@@ -124,12 +124,12 @@ class Template
 	}
 
 	/**
-	 * Throw always an Error exception, if this is called, this is because a 
+	 * Throw always an Error exception, if this is called, this is because a
 	 * non existant template data try to be used.
-	 * 
+	 *
 	 * @throws Error
 	 * @param string $key
-	 * @return null 
+	 * @return null
 	 */
     public function __get($key)
     {
@@ -139,57 +139,57 @@ class Template
     }
 
 	/**
-	 * Check if a tempalte data exists. Always return false on protected or 
+	 * Check if a tempalte data exists. Always return false on protected or
 	 * private data (eg. starting by a underscore).
-	 * 
+	 *
 	 * @param string $key
-	 * @return bool 
+	 * @return bool
 	 */
     public function __isset($key)
     {
         if ('_' != substr($key, 0, 1)) return isset($this->$key);
-		
+
         return false;
     }
 
 	/**
 	 * Call a template data as a method if it callable, else throw an Error
 	 * exception.
-	 * 
+	 *
 	 * @throws Error
 	 * @param string $name
 	 * @param array  $args
-	 * @return mixed 
+	 * @return mixed
 	 */
     public function __call($name, array $args)
     {
 		if (!isset($name)) throw Error::create(13, array($name));
-		
+
         if (!is_callable($this->$name)) throw Error::create(14, array($name));
-				
+
 		return call_user_func_array($this->$name, $args);
     }
 
 	/**
 	 * Add or Change a template data value. If the data is private or protected,
-	 * (eg. starting with a underscore) throw an Error Exception 
-	 * 
+	 * (eg. starting with a underscore) throw an Error Exception
+	 *
 	 * @throws Error
 	 * @param string $key
-	 * @param mixed $val 
+	 * @param mixed $val
 	 */
     public function __set($key, $val)
     {
         if ('_' == substr($key, 0, 1)) throw Error::create(15, array($key));
-		
+
         $this->$key = $val;
     }
 
 	/**
-	 * Remove a tempalte data if it's not a private or protected data (eg. 
+	 * Remove a tempalte data if it's not a private or protected data (eg.
 	 * starting with a underscore).
-	 * 
-	 * @param string $key 
+	 *
+	 * @param string $key
 	 */
     public function __unset($key)
     {
@@ -198,7 +198,7 @@ class Template
 
 	/**
 	 * Internal filter: Escape a string for html content
-	 * 
+	 *
 	 * @param string $string
 	 * @return string
 	 */
@@ -211,7 +211,7 @@ class Template
 
 	/**
 	 * Internal filter: check if a number is between $min and $max (included)
-	 * 
+	 *
 	 * @param mixed $number
 	 * @param mixed $min
 	 * @param mixed $max
@@ -222,15 +222,15 @@ class Template
 		Filter::assume_float($number);
 		Filter::assume_float($min);
 		Filter::assume_float($max);
-		
+
         return ($min <= $number && $number <= $max);
     }
 
 	/**
 	 * Internal filter: sort a array (keep key associated)
-	 * 
+	 *
 	 * @param array $array
-	 * @return array 
+	 * @return array
 	 */
     protected function _filter_array_sort(array $array)
     {
@@ -240,9 +240,9 @@ class Template
 
 	/**
 	 * Internal filter: sort a array in reverse order (keep key associated)
-	 * 
+	 *
 	 * @param array $array
-	 * @return array 
+	 * @return array
 	 */
     protected function _filter_array_rsort(array $array)
     {
@@ -252,21 +252,21 @@ class Template
 
 	/**
 	 * Internal filter: sort a array in natural order (keep key associated)
-	 * 
+	 *
 	 * @param array $array
-	 * @return array 
+	 * @return array
 	 */
     protected function _filter_array_nsort(array $array)
     {
         natcasesort($array);
         return $array;
     }
-	
+
 	/**
 	 * Internal filter: return the first item in an array or null
-	 * 
+	 *
 	 * @param array $array
-	 * @return mixed 
+	 * @return mixed
 	 */
     protected function _filter_array_first(array $array)
     {
@@ -278,9 +278,9 @@ class Template
 
 	/**
 	 * Internal filter: return the last item in an array or null
-	 * 
+	 *
 	 * @param array $array
-	 * @return mixed 
+	 * @return mixed
 	 */
     protected function _filter_array_last(array &$array)
     {
@@ -292,23 +292,23 @@ class Template
 
 	/**
 	 * Internal filter: return a default value if a data is not set.
-	 * 
+	 *
 	 * @param mixed $data
 	 * @param mixed $default
-	 * @return mixed 
+	 * @return mixed
 	 */
 	protected function _filter_default($data, $default)
 	{
 		return (isset($data)) ? $data : $default;
 	}
-	
+
 	/**
 	 * Internal filter: return $yes if $data is true else $no.
-	 * 
+	 *
 	 * @param mixed $data
 	 * @param mixed $yes
 	 * @param mixed $no
-	 * @return mixed 
+	 * @return mixed
 	 */
 	protected function _filter_if($data, $yes, $no)
 	{
@@ -317,7 +317,7 @@ class Template
 
 	/**
 	 * Internal filter: Format a DateTime object.
-	 * 
+	 *
 	 * @param DateTime $date
 	 * @param int $datetime
 	 * @param string $format
@@ -332,7 +332,7 @@ class Template
 				elseif ($datetime == 1)  $format = 'l jS \of F Y, h:i A';
 				else					 $format = 'l jS \of F Y';
 				break;
-			case 'longer' :  // Sat 8th of dec 2005, 03:04 PM 
+			case 'longer' :  // Sat 8th of dec 2005, 03:04 PM
 				if     ($datetime == 2) $format = 'h:i A';
 				elseif ($datetime == 1) $format = 'D jS \of M Y, h:i A';
 				else					$format = 'D jS \of M Y';
@@ -348,7 +348,7 @@ class Template
 				else					$format = 'd-m-Y';
 				break;
 		}
-		
+
 		$date->setTimezone(Config::create()->timezone);
 
 		return $date->format($format);
@@ -356,19 +356,19 @@ class Template
 
 	/**
 	 * Internal filter: Format a number to 2 decimal.
-	 * 
+	 *
 	 * @param mixed $data
-	 * @return string 
+	 * @return string
 	 */
 	protected function _filter_number($data)
 	{
 		return number_format($number, 2);
 	}
-	
+
 	/**
-	 * Compile a template into a raw template. (eg. Transform it into a valid 
+	 * Compile a template into a raw template. (eg. Transform it into a valid
 	 * php code without associating the template data to it).
-	 * 
+	 *
 	 * @param string $templateFile
 	 * @return string
 	 */
@@ -389,17 +389,17 @@ class Template
         $tpl->make_for();
         $tpl->make_inline();
 		$tpl->unescape_escape();
-        $tpl->unescape_raw_block();   
+        $tpl->unescape_raw_block();
 		$tpl->replace_php_short_open_tag();
-		
+
 		return $tpl->tpl;
-	}	
+	}
 
 	/**
 	 * Return a rendered view data into a template file.
-	 * $templateFile is a relative path to PLUG_PATH contant (eg. the plugin 
+	 * $templateFile is a relative path to PLUG_PATH contant (eg. the plugin
 	 * folder).
-	 * 
+	 *
 	 * @param string $templateFile
 	 * @return string
 	 */
@@ -410,7 +410,7 @@ class Template
         try
         {
 			$templateFile = str_replace('/', SEP, $templateFile);
-			
+
 			$tpl   = $this->_tplPath . $templateFile;
 			$cache = $this->_tplCache . $templateFile;
 
@@ -421,7 +421,7 @@ class Template
 			{
 				// create the raw content
 				$raw = $this->_compile($tpl);
-				
+
 				// cache is disabled; get a temp file; erase the cache filename
 				if (!$this->_cacheRawTemplate)
 				{
@@ -436,33 +436,33 @@ class Template
 
 				// store the raw content in the file (temp or cache)
 				file_put_contents($cache, $raw);
-				
+
 				// load the file
 				include $cache;
-				
+
 				// if cache is disabled we clean up
 				if (!$this->_cacheRawTemplate) unlink($cache);
 			}
 			// else load directly the cached file
-			else include $cache;				
+			else include $cache;
         }
         catch(\Exception $e) { ob_end_clean(); throw $e; }
 
-        return ob_get_clean();		
+        return ob_get_clean();
     }
 
 	/**
 	 * Print the rendered data into a template file.
-	 * $templateFile is a relative path to PLUG_PATH contant (eg. the plugin 
+	 * $templateFile is a relative path to PLUG_PATH contant (eg. the plugin
 	 * folder).
-	 * 
+	 *
 	 * @param string $templateFile
-	 * @return Template 
+	 * @return Template
 	 */
 	public function publish($templateFile)
 	{
 		echo $this->render($templateFile);
-		
+
 		return $this;
 	}
 }
