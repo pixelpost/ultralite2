7d05ba94791cf6f973251934adff0fffda802732
Alban LEROUX <seza@paradoxal.org>
2012-03-01 23:37:56 +0100

    Test: Add SqlMapper unit test. Core: Make SqlMapper work faster by remover Filter call and sprintf. (profiling)

--- app/core/SqlMapper.php
+++ app/core/SqlMapper.php
@@ -11,7 +11,7 @@ namespace pixelpost;
  * @version    0.0.1
  * @since      File available since Release 1.0.0
  */
-class SqlMapper 
+class SqlMapper
 {
 	const SQL_NULL = 0;
 	const SQL_TEXT = 1;
@@ -19,7 +19,7 @@ class SqlMapper
 	const SQL_REAL = 3;
 	const SQL_DATE = 4;
 	const SQL_BLOB = 5;
-	
+
 	const DATA_STRING = 0;
 	const DATA_INT    = 1;
 	const DATA_FLOAT  = 2;
@@ -31,31 +31,31 @@ class SqlMapper
 	protected $_dataMap   = array();
 	protected $_sqlMap    = array();
 	protected $_sqlType   = array();
-	
+
 	/**
 	 * Return an SqlMapper object
-	 * 
-	 * @return SqlMapper 
+	 *
+	 * @return SqlMapper
 	 */
 	public static function create()
 	{
 		return new SqlMapper();
 	}
-	
+
 	/**
-	 * Register a new field in the map, if sqlType is not provided he is 
+	 * Register a new field in the map, if sqlType is not provided he is
 	 * automatically created from $datatype with this casting rule :
-	 * 
+	 *
 	 * DATA_STRING => SQL_TEXT
 	 * DATA_INT    => SQL_INT
 	 * DATA_FLOAT  => SQL_REAL
 	 * DATA_BOOL   => SQL_INT
 	 * DATA_NULL   => SQL_NULL
-	 * 
+	 *
 	 * @param  string    $dataName
 	 * @param  string    $sqlField
 	 * @param  int       $dataType
-	 * @param  int       $sqlType 
+	 * @param  int       $sqlType
 	 * @return SqlMapper
 	 */
 	public function map($dataName, $sqlField, $dataType, $sqlType = null)
@@ -73,184 +73,152 @@ class SqlMapper
 				default                : $sqlType = self::SQL_NULL; break;
 			}
 		}
-		
+
 		$this->_dataTypes[$dataName] = $dataType;
 		$this->_dataMap[$dataName]   = $sqlField;
 		$this->_sqlMap[$sqlField]    = $dataName;
 		$this->_sqlType[$sqlField]   = $sqlType;
-		
+
 		return $this;
 	}
-	
+
+	/**
+	 * Cast SQL datatype into the $datatype provided
+	 *
+	 * @param  mixed $value
+	 * @param  int   $dataType
+	 * @return mixed
+	 */
+	protected function _castData($value, $dataType)
+	{
+		switch($dataType)
+		{
+			case self::DATA_STRING : return strval($value);
+			case self::DATA_INT    : return intval($value);
+			case self::DATA_FLOAT  : return floatval($value);
+			case self::DATA_BOOL   : return (bool) intval($value);
+			case self::DATA_DATE   : return Db::date_unserialize($value);
+			default                : return null;
+		}
+	}
+
+	/**
+	 * Cast Data datatype into the $sqltype provided
+	 *
+	 * @param  mixed $value
+	 * @param  int   $sqlType
+	 * @return mixed
+	 */
+	protected function _castSql($value, $sqlType)
+	{
+		switch($sqlType)
+		{
+			case self::SQL_TEXT : return Db::escape(strval($value));
+			case self::SQL_INT  : return intval($value);
+			case self::SQL_REAL : return floatval($value);
+			case self::SQL_BLOB : return Db::escape($value);
+			case self::SQL_DATE : return Db::date_serialize($value);
+			default             : return null;
+		}
+	}
+
 	/**
 	 * Create a comma separated SQL field list corresponding to the data field
 	 * provide in the $dataFields array
-	 * 
+	 *
 	 * @param  array  $dataFields
 	 * @return string
 	 */
 	public function genSqlSelectList(array $dataFields)
 	{
 		$list = array();
-		
+
 		foreach($dataFields as $dataField)
 		{
 			if (!isset($this->_dataMap[$dataField])) continue;
-			
+
 			$list[] = $this->_dataMap[$dataField];
 		}
-		
+
 		return implode(', ', $list);
 	}
-	
+
 	/**
 	 * Create a comma separated SQL field list corresponding to the data field
 	 * provide in the $dataFields array
-	 * 
+	 *
 	 * @param  array  $dataFields
 	 * @return string
 	 */
 	public function genSqlUpdateList(array $dataFields)
 	{
 		$list = array();
-		
+
 		foreach($dataFields as $dataField => $value)
 		{
 			if (!isset($this->_dataMap[$dataField])) continue;
-			
+
 			$sqlField = $this->_dataMap[$dataField];
 			$sqlType  = $this->_sqlType[$sqlField];
 			$sqlData  = $this->_castSql($value, $sqlType);
-			$list[] = sprintf('%s = %s', $sqlField, $sqlData);
+			$list[] = $sqlField . ' = ' . $sqlData;
 		}
-		
+
 		return implode(', ', $list);
 	}
-	
+
 	/**
 	 * Create a comma separated SQL field list corresponding to the data field
 	 * provide in the $dataFields array
-	 * 
+	 *
 	 * @param  array  $dataFields
 	 * @return string
 	 */
 	public function genSqlInsertList(array $dataFields)
 	{
 		$list = array();
-		
+
 		foreach($dataFields as $dataField => $value)
 		{
 			if (!isset($this->_dataMap[$dataField])) continue;
-			
+
 			$sqlField = $this->_dataMap[$dataField];
 			$sqlType  = $this->_sqlType[$sqlField];
 			$sqlData  = $this->_castSql($value, $sqlType);
 			$list[$sqlField] = $sqlData;
 		}
-		
+
 		return '(' . implode(', ', array_keys($list)) . ') VALUES (' . implode(', ', $list) . ')';
 	}
-	
+
 	/**
 	 * Transform a SQL array result in his corresponding data array result form.
-	 * 
-	 * The $todo closure permit to manipulate the resultset. This closure is 
+	 *
+	 * The $todo closure permit to manipulate the resultset. This closure is
 	 * called with one argument the resultset, consider to pass it by reference
 	 * to manipulate it.
-	 * 
+	 *
 	 * @param  array   $result
 	 * @param  Closure $todo
-	 * @return array 
+	 * @return array
 	 */
 	public function genArrayResult(array $result, \Closure $todo = null)
 	{
 		$list = array();
-		
+
 		foreach($result as $sqlField => $value)
 		{
 			if (!isset($this->_sqlMap[$sqlField])) continue;
-			
+
 			$dataField = $this->_sqlMap[$sqlField];
 			$dataType  = $this->_dataTypes[$dataField];
-			
+
 			$list[$dataField] = $this->_castData($value, $dataType);
 		}
 
 		if (!is_null($todo)) $todo($list);
-		
+
 		return $list;
-	}
-	
-	/**
-	 * Cast SQL datatype into the $datatype provided
-	 * 
-	 * @param  mixed $value
-	 * @param  int   $dataType
-	 * @return mixed 
-	 */
-	protected function _castData($value, $dataType)
-	{
-		switch($dataType)
-		{
-			case self::DATA_STRING :
-				Filter::assume_string($value);
-				return $value;
-			
-			case self::DATA_INT :
-				Filter::assume_int($value);
-				return $value;
-			
-			case self::DATA_FLOAT :
-				Filter::assume_float($value);
-				return $value;
-			
-			case self::DATA_BOOL :
-				Filter::assume_int($value);
-				Filter::assume_bool($value);
-				return $value;
-			
-			case self::DATA_DATE :				
-				return Db::date_unserialize($value);
-			
-			default :
-				return null;
-		}
-	}
-	
-	/**
-	 * Cast Data datatype into the $sqltype provided
-	 * 
-	 * @param  mixed $value
-	 * @param  int   $sqlType
-	 * @return mixed 
-	 */
-	protected function _castSql($value, $sqlType)
-	{
-		$db = Db::create();
-		
-		switch($sqlType)
-		{
-			case self::SQL_TEXT :
-				Filter::assume_string($value);
-				return Db::escape($value);
-			
-			case self::SQL_INT :
-				Filter::assume_int($value);
-				return $value;
-			
-			case self::SQL_REAL :
-				Filter::assume_float($value);
-				return $value;
-			
-			case self::SQL_BLOB :
-				return Db::escape($value);
-			
-			case self::SQL_DATE :				
-				return Db::date_serialize($value);
-			
-			default :
-				return null;
-		}
 	}
 }
 
--- test/app/core/SqlMapperTest.php
+++ test/app/core/SqlMapperTest.php
@@ -20,6 +20,12 @@ class SqlMapperTest extends \PHPUnit_Framework_TestCase
 	protected function setUp()
 	{
 		$this->object = new SqlMapper;
+
+		$this->object->map('name',      'username',  SqlMapper::DATA_STRING);
+		$this->object->map('total',     'count',     SqlMapper::DATA_INT);
+		$this->object->map('birth',     'birthday',  SqlMapper::DATA_DATE);
+		$this->object->map('last-seen', 'connected', SqlMapper::DATA_DATE);
+		$this->object->map('active',    'active',    SqlMapper::DATA_BOOL);
 	}
 
 	/**
@@ -28,73 +34,187 @@ class SqlMapperTest extends \PHPUnit_Framework_TestCase
 	 */
 	protected function tearDown()
 	{
-		
 	}
 
 	/**
-	 * @todo Implement testCreate().
+	 * @covers pixelpost\SqlMapper::create
 	 */
-	public function testCreate()
+	public function test_create()
 	{
-		// Remove the following lines when you implement this test.
-		$this->markTestIncomplete(
-				'This test has not been implemented yet.'
-		);
+		$this->assertTrue(SqlMapper::create() instanceOf SqlMapper);
 	}
 
 	/**
-	 * @todo Implement testMap().
+	 * @covers pixelpost\SqlMapper::map
 	 */
-	public function testMap()
+	public function test_map()
 	{
-		// Remove the following lines when you implement this test.
-		$this->markTestIncomplete(
-				'This test has not been implemented yet.'
-		);
+		$result = $this->object->map('data_name', 'sql_name', SqlMapper::DATA_STRING, SqlMapper::SQL_TEXT);
+
+		$this->assertTrue($result instanceOf SqlMapper);
 	}
 
 	/**
-	 * @todo Implement testGenSqlSelectList().
+	 * @covers pixelpost\SqlMapper::genSqlSelectList
 	 */
-	public function testGenSqlSelectList()
+	public function test_genSqlSelectList()
 	{
-		// Remove the following lines when you implement this test.
-		$this->markTestIncomplete(
-				'This test has not been implemented yet.'
-		);
+		$fields = array('name', 'active', 'birth');
+
+		$result = 'username, active, birthday';
+
+		$this->assertSame($result, $this->object->genSqlSelectList($fields));
 	}
 
 	/**
-	 * @todo Implement testGenSqlUpdateList().
+	 * @covers pixelpost\SqlMapper::genSqlSelectList
 	 */
-	public function testGenSqlUpdateList()
+	public function test_genSqlSelectList_unknown_field()
 	{
-		// Remove the following lines when you implement this test.
-		$this->markTestIncomplete(
-				'This test has not been implemented yet.'
+		$fields = array('name', 'active', 'foo', 'birth');
+
+		$result = 'username, active, birthday';
+
+		$this->assertSame($result, $this->object->genSqlSelectList($fields));
+	}
+
+
+	/**
+	 * @covers pixelpost\SqlMapper::genSqlUpdateList
+	 * @covers pixelpost\SqlMapper::_castSql
+	 */
+	public function test_genSqlUpdateList()
+	{
+		$fields = array(
+			'active' => true,
+			'birth'  => new \DateTime('2012-02-29 14:15:16', new \DateTimeZone('UTC')),
+			'name'   => 'Alban',
+			'total'  => 3,
 		);
+
+		$result = 'active = 1, birthday = 20120229141516, username = \'Alban\', count = 3';
+
+		$this->assertSame($result, $this->object->genSqlUpdateList($fields));
 	}
 
 	/**
-	 * @todo Implement testGenSqlInsertList().
+	 * @covers pixelpost\SqlMapper::genSqlUpdateList
+	 * @covers pixelpost\SqlMapper::_castSql
 	 */
-	public function testGenSqlInsertList()
+	public function test_genSqlUpdateList_unknown_field()
 	{
-		// Remove the following lines when you implement this test.
-		$this->markTestIncomplete(
-				'This test has not been implemented yet.'
+		$fields = array(
+			'active' => true,
+			'birth'  => new \DateTime('2012-02-29 14:15:16', new \DateTimeZone('UTC')),
+			'foo'    => 42,
+			'name'   => 'Alban',
+			'total'  => 3,
 		);
+
+		$result = 'active = 1, birthday = 20120229141516, username = \'Alban\', count = 3';
+
+		$this->assertSame($result, $this->object->genSqlUpdateList($fields));
 	}
 
 	/**
-	 * @todo Implement testGenArrayResult().
+	 * @covers pixelpost\SqlMapper::genSqlInsertList
+	 * @covers pixelpost\SqlMapper::_castSql
 	 */
-	public function testGenArrayResult()
+	public function test_genSqlInsertList()
 	{
-		// Remove the following lines when you implement this test.
-		$this->markTestIncomplete(
-				'This test has not been implemented yet.'
+		$fields = array(
+			'active' => true,
+			'birth'  => new \DateTime('2012-02-29 14:15:16', new \DateTimeZone('UTC')),
+			'name'   => 'Alban',
+			'total'  => 3,
 		);
+
+		$result = '(active, birthday, username, count) VALUES (1, 20120229141516, \'Alban\', 3)';
+
+		$this->assertSame($result, $this->object->genSqlInsertList($fields));
 	}
 
+	/**
+	 * @covers pixelpost\SqlMapper::genSqlInsertList
+	 * @covers pixelpost\SqlMapper::_castSql
+	 */
+	public function test_genSqlInsertList_unknown_field()
+	{
+		$fields = array(
+			'active' => true,
+			'birth'  => new \DateTime('2012-02-29 14:15:16', new \DateTimeZone('UTC')),
+			'foo'    => 42,
+			'name'   => 'Alban',
+			'total'  => 3,
+		);
+
+		$result = '(active, birthday, username, count) VALUES (1, 20120229141516, \'Alban\', 3)';
+
+		$this->assertSame($result, $this->object->genSqlInsertList($fields));
+	}
+
+	/**
+	 * @covers pixelpost\SqlMapper::genArrayResult
+	 * @covers pixelpost\SqlMapper::_castData
+	 */
+	public function test_genArrayResult()
+	{
+		$result = array(
+			'birthday' => '20120229141516',
+			'active'   => '0',
+			'username' => 'Bertrand',
+			'count'    => '18',
+		);
+
+		$test = array(
+			'birth'  => '2012-02-29 14:15:16',
+			'active' => false,
+			'name'   => 'Bertrand',
+			'total'  => 18,
+		);
+
+		$me = $this;
+
+		$todo = function(&$result) use ($me)
+		{
+			$me->assertTrue($result['birth'] instanceOf \DateTime);
+
+			$result['birth'] = $result['birth']->format('Y-m-d H:i:s');
+		};
+
+		$this->assertSame($test, $this->object->genArrayResult($result, $todo));
+	}
+
+	/**
+	 * @covers pixelpost\SqlMapper::genArrayResult
+	 * @covers pixelpost\SqlMapper::_castData
+	 */
+	public function test_genArrayResult_unknown_field()
+	{
+		$result = array(
+			'birthday' => '20120229141516',
+			'active'   => '0',
+			'foo'      => 'bar',
+			'username' => 'Bertrand',
+			'count'    => '18',
+		);
+
+		$test = array(
+			'birth'  => '2012-02-29 14:15:16',
+			'active' => false,
+			'name'   => 'Bertrand',
+			'total'  => 18,
+		);
+
+		$me = $this;
+
+		$todo = function(&$result) use ($me)
+		{
+			$me->assertTrue($result['birth'] instanceOf \DateTime);
+
+			$result['birth'] = $result['birth']->format('Y-m-d H:i:s');
+		};
+
+		$this->assertSame($test, $this->object->genArrayResult($result, $todo));
+	}
 }
