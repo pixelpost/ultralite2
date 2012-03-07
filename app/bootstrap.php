0c1e6191a592a2cac299c365792562516c28998e
Alban LEROUX <seza@paradoxal.org>
2012-03-07 15:39:01 +0100

    Core: Error reporting 5.4 compliant
    Additionaly:
    – Rewrite commit a96c9b6aa552344dd7b213554cea1d36fecd028f according last discussion;
    – Spaces to tabs indentation;
    – Remove unecessary magic_quotes ini settings.

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,7 +4,9 @@
 namespace pixelpost;
 
 // Step 1. A little of PHP conf
-error_reporting(E_ALL | E_STRICT);
+
+// report always all error (-1 is better than constant, php 5.3 & 5.4 compliant)
+error_reporting(-1);
 
 ini_set('date.timezone',                 'UTC');
 ini_set('default_socket_timeout',        '10');
@@ -23,12 +25,12 @@ ini_set('register_argc_argv',            'off');
 ini_set('register_long_arrays',          'off');
 ini_set('safe_mode',                     'off');
 ini_set('short_open_tag',                'off');
-ini_set('magic_quotes_gpc',              'off');
 ini_set('display_errors',                'stdout');
 ini_set('html_errors',                   'off');
 
+
 // Step 2. A little of constant creation
-defined('VERSION')   or define('VERSION',   "0.0.1",                     true);
+defined('VERSION')   or define('VERSION',   '0.0.1',                     true);
 defined('SEP')       or define('SEP',       DIRECTORY_SEPARATOR,         true);
 defined('ROOT_PATH') or define('ROOT_PATH', dirname(__DIR__),            true);
 defined('APP_PATH')  or define('APP_PATH',  ROOT_PATH . SEP . 'app',     true);
@@ -44,16 +46,18 @@ set_error_handler(function ($errno, $errstr, $errfile, $errline)
 
 set_exception_handler(function ($exception)
 {
+	$debug = !defined('DEBUG') or DEBUG;
+
 	if (class_exists('\pixelpost\Event'))
 	{
-		$event = \pixelpost\Event::signal('error.new', array('exception' => $exception));
+		$event = \pixelpost\Event::signal('error.new', compact('exception'));
 
-		if (!$event->is_processed() && (!defined('DEBUG') || DEBUG))
+		if (!$event->is_processed() && $debug)
 		{
 			echo $exception;
 		}
 	}
-	elseif (!defined('DEBUG') || DEBUG)
+	elseif ($debug)
 	{
 		echo $exception;
 	}
@@ -62,45 +66,45 @@ set_exception_handler(function ($exception)
 // Step 4. We need a cool autoloader
 spl_autoload_register(function($className)
 {
-    // the main namespace, all other is ignored
-    $nsPrefix = 'pixelpost\\';
+	// the main namespace, all other is ignored
+	$nsPrefix = 'pixelpost\\';
 
 	// we need to keep $className, so we work on $class
 	$class    = $className;
 
-    // some security checking
-    if (strpos($class, '/') !== false) return false;
-    if (strpos($class, '.') !== false) return false;
+	// some security checking
+	if (strpos($class, '/') !== false) return false;
+	if (strpos($class, '.') !== false) return false;
 
-    // remove the beginning backslash
-    if (substr($class, 0, 1) == '\\') $class = substr($class, 1);
+	// remove the beginning backslash
+	if (substr($class, 0, 1) == '\\') $class = substr($class, 1);
 
-    // check if the class start with the main namespace
-    if (substr($class, 0, strlen($nsPrefix)) != $nsPrefix) return false;
+	// check if the class start with the main namespace
+	if (substr($class, 0, strlen($nsPrefix)) != $nsPrefix) return false;
 
-    // remove the main namespace of the class name
-    $class = substr($class, strlen($nsPrefix));
+	// remove the main namespace of the class name
+	$class = substr($class, strlen($nsPrefix));
 
-    // get all parts of the class name
-    $items = explode('\\', $class);
+	// get all parts of the class name
+	$items = explode('\\', $class);
 
-    // extract the file name
-    $class = array_pop($items);
+	// extract the file name
+	$class = array_pop($items);
 
-    // create the related path
-    $path = (count($items) == 0) ? 'Core' : implode('\\', $items);
+	// create the related path
+	$path = (count($items) == 0) ? 'Core' : implode(SEP, $items);
 
-    // create the absolute path with the complete file name
-    $file = APP_PATH . SEP . str_replace('\\', SEP, $path) . SEP . $class . '.php';
+	// create the absolute path with the complete file name
+	$file = APP_PATH . SEP . $path . SEP . $class . '.php';
 
-    // check if file exists...
-    if ( ! is_file($file)) return false;
+	// check if file exists...
+	if (!is_file($file)) return false;
 
-    // include him...
-    require_once $file;
+	// include him...
+	require_once $file;
 
-    // return if the class is loaded...
-    return class_exists($className);
+	// return if the class is loaded...
+	return class_exists($className);
 });
 
 // Step 5. We need to parse the config file and set properly the environnement
@@ -108,12 +112,9 @@ $conf = Config::load(PRIV_PATH . SEP . 'config.json');
 
 // Debug can also be switched on by setting the Apache envrionment
 // variable `APPLICATION_ENV` to `development` in .htaccess
-if (!defined('DEBUG') && isset($_SERVER['APPLICATION_ENV']) &&
-	$_SERVER['APPLICATION_ENV'] == 'development')
-	define('DEBUG', true, true);
-else
-	define('DEBUG', $conf->debug, true);
+$debug = ($conf->debug or 'development' == getenv('APPLICATION_ENV'));
 
+defined('DEBUG')       or define('DEBUG',       $debug, true);
 defined('WEB_URL')     or define('WEB_URL',     $conf->url,   true);
 defined('CONTENT_URL') or define('CONTENT_URL', $conf->url . 'app/plugins/',   true);
 
