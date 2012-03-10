320f0bd167db770cb05a8504698ab56dd7b736b6
Alban LEROUX <seza@paradoxal.org>
2012-03-10 14:38:04 +0100

    See previous commit, this file was not commited why???…
    Personnal: Git is obviously a strange unusefull thing compared to Hg.

--- app/plugins/auth/api/auth_user_get.php
+++ app/plugins/auth/api/auth_user_get.php
@@ -18,8 +18,8 @@ if (!Plugin::is_granted('admin')) throw new Ungranted($method);
 $user = self::get_required('user', $request, $method);
 
 // check user exists
-if (!self::check_user_name($user)) throw new FieldNonExists('user');
+if (!self::check_user_name($user, $id, $pass, $mail)) throw new FieldNonExists('user');
 
 // this can feel stupid but it's important to keep a distinction between
 // user identifier and user name, even if today it is the same.
-$event->response = array('name' => $user);
+$event->response = array('name' => $user, 'email' => $mail);
