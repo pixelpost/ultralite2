pixelpost/ultralite2

commit c84199cf33d3562694a5325eb7d66cfcf5b2018d

Plugin Auth: Massive change, need reinstall, see below
– User now have email.
– Add entity support.
– Add session support.
– Token now have expire date.
– Add API auth.destroy method.
– Add API auth.entity_add method.
– Add API auth.entity_del method.
– Add API auth.entity_get method.
– Add API auth.entity_set method.
– Add API auth.entity_list method.
– Add API auth.entity_grant_add method.
– Add API auth.entity_grant_del method.

API changes:
– auth.request
– auth.user.add
– auth.user.get
– auth.user.set
– auth.user.list
– auth.grant.list

app/plugins/auth/api/auth_refresh.php
@@ -7,7 +7,8 @@ use pixelpost\Config,
 	pixelpost\plugins\api\Exception\FieldNotValid;
 
 // check the request
-if (!isset($event->request->token)) throw new FieldRequired('auth.refresh', 'token');
+if (!isset($event->request->token))     throw new FieldRequired('auth.refresh', 'token');
+if (!isset($event->request->signature)) throw new FieldRequired('auth.refresh', 'signature');
 
 // check the token exists
 try
@@ -19,20 +20,17 @@ catch(ModelExceptionNoResult $e)
 	throw new FieldNotValid('token');
 }
 
-// retrieve user and the password correspondig to the token
-$user = Model::user_get_by_id($token['user_id']);
+// retrieve entity public and private key correspondig to the challenge
+$entity = Model::entity_get_by_id($challenge['entity_id']);
 
-// retrieve configuration
-$conf     = Config::create();
-$lifetime = $conf->plugin_auth->lifetime;
-$key      = $conf->uid;
+// retrieve the lifetime configuration value
+$lifetime = Config::create()->plugin_auth->lifetime;
 
 // set auth module
 $auth = new Auth();
 $auth->set_lifetime($lifetime)
-	 ->set_key($key)
-	 ->set_username($user['name'])
-	 ->set_password_hash($user['pass'])
+	 ->set_public_key($entity['public_key'])
+	 ->set_private_key($entity['private_key'])
 	 ->set_challenge($token['challenge']);
 
 // generate new token, nonce and check the request signature
@@ -41,13 +39,10 @@ $new_token = $auth->get_token();
 $sign      = $auth->get_signature($event->request->token);
 $signature = $auth->get_signature($nonce);
 
-if ($sign != $event->request->signature)
-{
-	throw new FieldNotValid('signature');
-}
+if ($sign != $event->request->signature) throw new FieldNotValid('signature');
 
 // store the token in database
-Model::token_update_token($token['id'], $new_token);
+Model::token_update_token($token['id'], $new_token, $lifetime);
 Model::token_update_nonce($token['id'], $nonce);
 
 // return the response

app/plugins/auth/Admin.php
@@ -12,8 +12,7 @@ class Admin
 	public static function template_footer(Event $event)
 	{
 		$event->response[] = Template::create()
-				->assign('key', Config::create()->uid)
-				->assign('user', Plugin::get_username())
+				->assign('user', Plugin::get_entity_name())
 				->render('auth/tpl/admin-footer.php');
 	}
 

app/plugins/auth/Api.php
@@ -2,7 +2,10 @@
 
 namespace pixelpost\plugins\auth;
 
-use pixelpost\Event;
+use pixelpost\Event,
+	pixelpost\Filter,
+	pixelpost\plugins\api\Exception\FieldRequired,
+	pixelpost\plugins\api\Exception\FieldEmpty;
 
 /**
  * Provide API methods for managing auth content
@@ -15,6 +18,114 @@ use pixelpost\Event;
 class Api
 {
 
+	/**
+	 * Verify that a grant name exists.
+	 * Becareful, next arguments are references.
+	 *
+	 * @param  string $name The grant name
+	 * @param  int    $id   Will be set to the grant id
+	 * @return bool
+	 **/
+	protected static function check_grant_name($name, &$id = null)
+	{
+		try
+		{
+			// create $id
+			extract(Model::grant_get_by_name($name));
+			return true;
+		}
+		catch(ModelExceptionNoResult $e)
+		{
+			return false;
+		}
+	}
+
+	/**
+	 * Verify that a user name exists.
+	 * Becareful, next arguments are references.
+	 *
+	 * @param  string $name  The user name
+	 * @param  int    $id    Will be set to the user id
+	 * @param  string $pass  Will be set to the user password
+	 * @param  string $email Will be set to the user email
+	 * @return bool
+	 **/
+	protected static function check_user_name($name, &$id = null, &$pass = null, &$email = null)
+	{
+		try
+		{
+			// create $id, $pass and $email
+			extract(Model::user_get_by_name($name));
+			return true;
+		}
+		catch(ModelExceptionNoResult $e)
+		{
+			return false;
+		}
+	}
+
+	/**
+	 * Verify that a entity key exists.
+	 * Becareful, next arguments are references.
+	 *
+	 * @param  string $key     The entity key
+	 * @param  int    $id      Will be set to the entity id
+	 * @param  int    $user_id Will be set to the user id
+	 * @return bool
+	 **/
+	protected static function check_entity_key($key, &$id = null, &$user_id = null)
+	{
+		try
+		{
+			// create id, name, user_id, private_key
+			extract(Model::entity_get_by_public_key($key));
+			return true;
+		}
+		catch(ModelExceptionNoResult $e)
+		{
+			return false;
+		}
+	}
+
+	/**
+	 * Retrieve a field from a request.
+	 *
+	 * @param  string   $field   The field name to retrieve
+	 * @param  stdClass $request The event request
+	 * @param  string   $method  The method name
+	 * @return mixed
+	 **/
+	protected static function get_required($field, $request, $method)
+	{
+		$data = isset($request->$field) ? trim($request->$field) : false;
+
+		if ($data === false) throw new FieldRequired($method, $field);
+		if ($data === '')    throw new FieldEmpty($field);
+
+		Filter::check_encoding($data);
+
+		return $data;
+	}
+
+	/**
+	 * Retrieve a field from a request, if exists else return False.
+	 *
+	 * @param  string   $field   The field name to retrieve
+	 * @param  stdClass $request The event request
+	 * @param  string   $method  The method name
+	 * @return mixed
+	 **/
+	protected static function get_optional($field, $request, $method)
+	{
+		$data = isset($request->$field) ? trim($request->$field) : false;
+
+		if ($data === '') throw new FieldEmpty($field);
+
+		$data and Filter::check_encoding($data);
+
+		return $data;
+	}
+
 	public static function auth_version(Event $event)
 	{
 		$event->response = array('version' => Plugin::version());
@@ -22,86 +133,126 @@ class Api
 
 	public static function auth_request(Event $event)
 	{
-		include __DIR__ . SEP . 'api' . SEP . 'auth_request.php';
+		require __DIR__ . SEP . 'api' . SEP . 'auth_request.php';
 	}
 
 	public static function auth_token(Event $event)
 	{
-		include __DIR__ . SEP . 'api' . SEP . 'auth_token.php';
+		require __DIR__ . SEP . 'api' . SEP . 'auth_token.php';
 	}
 
 	public static function auth_refresh(Event $event)
 	{
-		include __DIR__ . SEP . 'api' . SEP . 'auth_refresh.php';
+		require __DIR__ . SEP . 'api' . SEP . 'auth_refresh.php';
+	}
+
+	public static function auth_destroy(Event $event)
+	{
+		require __DIR__ . SEP . 'api' . SEP . 'auth_destroy.php';
 	}
 
 	public static function auth_config_get(Event $event)
 	{
-		include __DIR__ . SEP . 'api' . SEP . 'auth_config_get.php';
+		require __DIR__ . SEP . 'api' . SEP . 'auth_config_get.php';
 	}
 
 	public static function auth_config_set(Event $event)
 	{
-		include __DIR__ . SEP . 'api' . SEP . 'auth_config_set.php';
+		require __DIR__ . SEP . 'api' . SEP . 'auth_config_set.php';
 	}
 
 	public static function auth_user_add(Event $event)
 	{
-		include __DIR__ . SEP . 'api' . SEP . 'auth_user_add.php';
+		require __DIR__ . SEP . 'api' . SEP . 'auth_user_add.php';
 	}
 
 	public static function auth_user_set(Event $event)
 	{
-		include __DIR__ . SEP . 'api' . SEP . 'auth_user_set.php';
+		require __DIR__ . SEP . 'api' . SEP . 'auth_user_set.php';
 	}
 
 	public static function auth_user_get(Event $event)
 	{
-		include __DIR__ . SEP . 'api' . SEP . 'auth_user_get.php';
+		require __DIR__ . SEP . 'api' . SEP . 'auth_user_get.php';
 	}
 
 	public static function auth_user_del(Event $event)
 	{
-		include __DIR__ . SEP . 'api' . SEP . 'auth_user_del.php';
+		require __DIR__ . SEP . 'api' . SEP . 'auth_user_del.php';
 	}
 
 	public static function auth_user_list(Event $event)
 	{
-		include __DIR__ . SEP . 'api' . SEP . 'auth_user_list.php';
+		require __DIR__ . SEP . 'api' . SEP . 'auth_user_list.php';
 	}
 
 	public static function auth_user_grant_add(Event $event)
 	{
-		include __DIR__ . SEP . 'api' . SEP . 'auth_user_grant_add.php';
+		require __DIR__ . SEP . 'api' . SEP . 'auth_user_grant_add.php';
 	}
 
 	public static function auth_user_grant_del(Event $event)
 	{
-		include __DIR__ . SEP . 'api' . SEP . 'auth_user_grant_del.php';
+		require __DIR__ . SEP . 'api' . SEP . 'auth_user_grant_del.php';
+	}
+
+	public static function auth_entity_add(Event $event)
+	{
+		require __DIR__ . SEP . 'api' . SEP . 'auth_entity_add.php';
+	}
+
+	public static function auth_entity_set(Event $event)
+	{
+		require __DIR__ . SEP . 'api' . SEP . 'auth_entity_set.php';
+	}
+
+	public static function auth_entity_get(Event $event)
+	{
+		require __DIR__ . SEP . 'api' . SEP . 'auth_entity_get.php';
+	}
+
+	public static function auth_entity_del(Event $event)
+	{
+		require __DIR__ . SEP . 'api' . SEP . 'auth_entity_del.php';
+	}
+
+	public static function auth_entity_list(Event $event)
+	{
+		require __DIR__ . SEP . 'api' . SEP . 'auth_entity_list.php';
+	}
+
+	public static function auth_entity_grant_add(Event $event)
+	{
+		require __DIR__ . SEP . 'api' . SEP . 'auth_entity_grant_add.php';
+	}
+
+	public static function auth_entity_grant_del(Event $event)
+	{
+		require __DIR__ . SEP . 'api' . SEP . 'auth_entity_grant_del.php';
 	}
 
 	public static function auth_grant_add(Event $event)
 	{
-		include __DIR__ . SEP . 'api' . SEP . 'auth_grant_add.php';
+		require __DIR__ . SEP . 'api' . SEP . 'auth_grant_add.php';
 	}
 
 	public static function auth_grant_set(Event $event)
 	{
-		include __DIR__ . SEP . 'api' . SEP . 'auth_grant_set.php';
+		require __DIR__ . SEP . 'api' . SEP . 'auth_grant_set.php';
 	}
 
 	public static function auth_grant_get(Event $event)
 	{
-		include __DIR__ . SEP . 'api' . SEP . 'auth_grant_get.php';
+		require __DIR__ . SEP . 'api' . SEP . 'auth_grant_get.php';
 	}
 
 	public static function auth_grant_del(Event $event)
 	{
-		include __DIR__ . SEP . 'api' . SEP . 'auth_grant_del.php';
+		require __DIR__ . SEP . 'api' . SEP . 'auth_grant_del.php';
 	}
 
 	public static function auth_grant_list(Event $event)
 	{
-		include __DIR__ . SEP . 'api' . SEP . 'auth_grant_list.php';
+		require __DIR__ . SEP . 'api' . SEP . 'auth_grant_list.php';
 	}
 }

app/plugins/auth/Auth.php
@@ -15,12 +15,11 @@ use stdClass,
  */
 class Auth
 {
-	protected $_lifetime  = 500;
-	protected $_username  = '';
-	protected $_password  = '';
-	protected $_key       = '';
-	protected $_challenge = '';
-	protected $_nonce     = '';
+	protected $_lifetime    = 500;
+	protected $_public_key  = '';
+	protected $_private_key = '';
+	protected $_challenge   = '';
+	protected $_nonce       = '';
 
 	/**
 	 * Change the auth lifetime
@@ -38,46 +37,31 @@ class Auth
 	}
 
 	/**
-	 * Set the auth username
+	 * Set the auth public key
 	 *
-	 * @param string $username The user name
+	 * @param string $public_key
 	 * @return pixelpost\plugins\auth\Auth
 	 */
-	public function set_username($username)
+	public function set_public_key($public_key)
 	{
-		Filter::is_string($username);
+		Filter::is_string($public_key);
 
-		$this->_username = $username;
+		$this->_public_key = $public_key;
 
 		return $this;
 	}
 
 	/**
-	 * Change the auth password (md5 hash)
+	 * Set the auth private key
 	 *
-	 * @param string $password The MD5 hash of the user password
+	 * @param string $private_key
 	 * @return pixelpost\plugins\auth\Auth
 	 */
-	public function set_password_hash($password)
+	public function set_private_key($private_key)
 	{
-		Filter::is_string($password);
+		Filter::is_string($private_key);
 
-		$this->_password = $password;
-
-		return $this;
-	}
-
-	/**
-	 * Set the auth secret key
-	 *
-	 * @param string $key The secret shared by both parts
-	 * @return pixelpost\plugins\auth\Auth
-	 */
-	public function set_key($key)
-	{
-		Filter::is_string($key);
-
-		$this->_key = $key;
+		$this->_private_key = $private_key;
 
 		return $this;
 	}
@@ -123,17 +107,17 @@ class Auth
 	}
 
 	/**
-	 * Generate and return the secret - Based on username, password, key
+	 * Generate and return the secret - Based on public_key, private_key
 	 *
 	 * @return string
 	 */
 	public function get_secret()
 	{
-		return md5($this->_username . $this->_password . $this->_key);
+		return md5($this->_public_key . $this->_private_key);
 	}
 
 	/**
-	 * Generate and return a challenge - Based on username, password, key
+	 * Generate and return a challenge - Based on public_key, private_key
 	 *
 	 * @return string
 	 */
@@ -153,7 +137,7 @@ class Auth
 	}
 
 	/**
-	 * Generate and return a token - Based on username, password, key, lifetime, challenge
+	 * Generate and return a token - Based on public_key, private_key, lifetime, challenge
 	 *
 	 * @return string
 	 */
@@ -163,7 +147,7 @@ class Auth
 	}
 
 	/**
-	 * Generate and return a hmac - Based on username, password, key, nonce
+	 * Generate and return a hmac - Based on public_key, private_key, nonce
 	 *
 	 * @param string $data The $data to hmac
 	 * @return string
@@ -174,7 +158,7 @@ class Auth
 	}
 
 	/**
-	 * Generate and return a signature - Based on username, password, key, lifetime, challenge
+	 * Generate and return a signature - Based on public_key, private_key, lifetime, challenge
 	 *
 	 * @param string $data The $data to sign
 	 * @return string

app/plugins/auth/Plugin.php
@@ -35,19 +35,29 @@ class Plugin implements PluginInterface
 	protected static $_api_nonce = '';
 
 	/**
-	 * @var string The authentified username if auth success
-	 */
-	protected static $_user_name = '';
-
-	/**
-	 * @var string The authentified password hashed if auth success
-	 */
-	protected static $_user_pass = '';
-
-	/**
-	 * @var int The authentified user_id if auth success
+	 * @var int The authenticated user_id if auth success
 	 */
 	protected static $_user_id  = 0;
+
+	/**
+	 * @var int The authenticated entity_id if auth success
+	 */
+	protected static $_entity_id = 0;
+
+	/**
+	 * @var string The authenticated entity public key if auth success
+	 */
+	protected static $_entity_pub = '';
+
+	/**
+	 * @var string The authenticated entity private key if auth success
+	 */
+	protected static $_entity_priv = '';
+
+	/**
+	 * @var int The authenticated entity name if auth success
+	 */
+	protected static $_entity_name = '';
 
 	public static function version()
 	{
@@ -106,24 +116,32 @@ class Plugin implements PluginInterface
 		// check admin auth before admin event method is called
 		Event::register('request.admin',    $selfClass . '::request_admin', 99);
 
-		Event::register('api.auth.version',        $apiClass . '::auth_version');
-		Event::register('api.auth.request',        $apiClass . '::auth_request');
-		Event::register('api.auth.token',          $apiClass . '::auth_token');
-		Event::register('api.auth.refresh',        $apiClass . '::auth_refresh');
-		Event::register('api.auth.config.get',     $apiClass . '::auth_config_get');
-		Event::register('api.auth.config.set',     $apiClass . '::auth_config_set');
-		Event::register('api.auth.user.add',       $apiClass . '::auth_user_add');
-		Event::register('api.auth.user.set',       $apiClass . '::auth_user_set');
-		Event::register('api.auth.user.get',       $apiClass . '::auth_user_get');
-		Event::register('api.auth.user.del',       $apiClass . '::auth_user_del');
-		Event::register('api.auth.user.list',      $apiClass . '::auth_user_list');
-		Event::register('api.auth.grant.add',      $apiClass . '::auth_grant_add');
-		Event::register('api.auth.grant.set',      $apiClass . '::auth_grant_set');
-		Event::register('api.auth.grant.get',      $apiClass . '::auth_grant_get');
-		Event::register('api.auth.grant.del',      $apiClass . '::auth_grant_del');
-		Event::register('api.auth.grant.list',     $apiClass . '::auth_grant_list');
-		Event::register('api.auth.user.grant.add', $apiClass . '::auth_user_grant_add');
-		Event::register('api.auth.user.grant.del', $apiClass . '::auth_user_grant_del');
+		Event::register('api.auth.version',          $apiClass . '::auth_version');
+		Event::register('api.auth.request',          $apiClass . '::auth_request');
+		Event::register('api.auth.token',            $apiClass . '::auth_token');
+		Event::register('api.auth.refresh',          $apiClass . '::auth_refresh');
+		Event::register('api.auth.destroy',          $apiClass . '::auth_destroy');
+		Event::register('api.auth.config.get',       $apiClass . '::auth_config_get');
+		Event::register('api.auth.config.set',       $apiClass . '::auth_config_set');
+		Event::register('api.auth.user.add',         $apiClass . '::auth_user_add');
+		Event::register('api.auth.user.set',         $apiClass . '::auth_user_set');
+		Event::register('api.auth.user.get',         $apiClass . '::auth_user_get');
+		Event::register('api.auth.user.del',         $apiClass . '::auth_user_del');
+		Event::register('api.auth.user.list',        $apiClass . '::auth_user_list');
+		Event::register('api.auth.entity.add',       $apiClass . '::auth_entity_add');
+		Event::register('api.auth.entity.set',       $apiClass . '::auth_entity_set');
+		Event::register('api.auth.entity.get',       $apiClass . '::auth_entity_get');
+		Event::register('api.auth.entity.del',       $apiClass . '::auth_entity_del');
+		Event::register('api.auth.entity.list',      $apiClass . '::auth_entity_list');
+		Event::register('api.auth.grant.add',        $apiClass . '::auth_grant_add');
+		Event::register('api.auth.grant.set',        $apiClass . '::auth_grant_set');
+		Event::register('api.auth.grant.get',        $apiClass . '::auth_grant_get');
+		Event::register('api.auth.grant.del',        $apiClass . '::auth_grant_del');
+		Event::register('api.auth.grant.list',       $apiClass . '::auth_grant_list');
+		Event::register('api.auth.user.grant.add',   $apiClass . '::auth_user_grant_add');
+		Event::register('api.auth.user.grant.del',   $apiClass . '::auth_user_grant_del');
+		Event::register('api.auth.entity.grant.add', $apiClass . '::auth_entity_grant_add');
+		Event::register('api.auth.entity.grant.del', $apiClass . '::auth_entity_grant_del');
 
 		Event::register('admin.template.footer', $adminClass . '::template_footer');
 		Event::register('admin.template.css',    $adminClass . '::template_css');
@@ -134,7 +152,7 @@ class Plugin implements PluginInterface
 	}
 
 	/**
-	 * Verify if a user is authentified for admin pages. if not print the login
+	 * Verify if a user is authenticated for admin pages. if not print the login
 	 * pages and break the request.admin chain (cause the original admin page
 	 * called is not generated).
 	 *
@@ -168,9 +186,9 @@ class Plugin implements PluginInterface
 			if (WebAuth::check($id, $name))
 			{
 				// register the identification (permit to internal api call to be
-				// authentified too).
-				self::$_user_id   = $id;
-				self::$_user_name = $name;
+				// authenticated too).
+				self::$_user_id     = $id;
+				self::$_entity_name = $name;
 				// call admin page
 				return true;
 			}
@@ -185,17 +203,47 @@ class Plugin implements PluginInterface
 	}
 
 	/**
-	 * Return the authentified username or an empty string
+	 * Return the authenticated entity name or an empty string
 	 *
 	 * @return string
 	 */
-	public static function get_username()
+	public static function get_entity_name()
 	{
-		return self::$_user_name;
+		return self::$_entity_name;
 	}
 
 	/**
-	 * Return the authentified user_id or 0
+	 * Return the authenticated entity public key or an empty string
+	 *
+	 * @return string
+	 */
+	public static function get_entity_public_key()
+	{
+		return self::$_entity_pub;
+	}
+
+	/**
+	 * Return the authenticated token id or 0
+	 *
+	 * @return int
+	 */
+	public static function get_token_id()
+	{
+		return self::$_api_token;
+	}
+
+	/**
+	 * Return the authenticated entity id or 0
+	 *
+	 * @return int
+	 */
+	public static function get_entity_id()
+	{
+		return self::$_entity_id;
+	}
+
+	/**
+	 * Return the authenticated user_id or 0
 	 *
 	 * @return string
 	 */
@@ -205,14 +253,25 @@ class Plugin implements PluginInterface
 	}
 
 	/**
-	 * Return if a user is authentified
+	 * Return if there is an authentication
 	 *
 	 * @return bool
 	 */
 	public static function is_auth()
 	{
-		// check if user is allready authentified
-		if (self::$_user_id != 0) return true;
+		// check if user is already authenticated
+		return (self::$_user_id != 0);
+	}
+
+	/**
+	 * Return if a user is authenticated
+	 *
+	 * @return bool
+	 */
+	public static function is_auth_admin()
+	{
+		// check if it is a entity which is logged
+		return (self::$_entity_id == 0);
 	}
 
 	/**
@@ -238,8 +297,8 @@ class Plugin implements PluginInterface
 			throw new ApiException('bad_token', 'The token is not valid.');
 		}
 
-		// retrieve user data
-		$user = Model::user_get_by_id($token['user_id']);
+		// retrieve entity data
+		$entity = Model::entity_get_by_id($token['entity_id']);
 
 		// retrieve configuration
 		$conf = Config::create();
@@ -247,9 +306,8 @@ class Plugin implements PluginInterface
 		// prepare auth class
 		$auth = new Auth();
 		$auth->set_lifetime($conf->plugin_auth->lifetime)
-			 ->set_key($conf->uid)
-			 ->set_username($user['name'])
-			 ->set_password_hash($user['pass'])
+			 ->set_public_key($entity['public_key'])
+			 ->set_private_key($entity['private_key'])
 			 ->set_challenge($token['challenge'])
 			 ->set_nonce($token['nonce']);
 
@@ -269,12 +327,13 @@ class Plugin implements PluginInterface
 			throw new ApiException('bad_hmac', 'The hmac is not valid.');
 		}
 
-		// store authentified username and id, generate a new nonce
-		self::$_api_token  = $token['id'];
-		self::$_api_nonce  = $token['nonce'];
-		self::$_user_id    = $token['user_id'];
-		self::$_user_name  = $user['name'];
-		self::$_user_pass  = $user['pass'];
+		// store authenticated entityname and id, generate a new nonce
+		self::$_api_token   = $token['id'];
+		self::$_api_nonce   = $token['nonce'];
+		self::$_entity_id   = $token['entity_id'];
+		self::$_entity_pub  = $entity['public_key'];
+		self::$_entity_priv = $entity['private_key'];
+		self::$_user_id     = $entity['user_id'];
 	}
 
 	/**
@@ -288,9 +347,8 @@ class Plugin implements PluginInterface
 
 		// prepare auth class
 		$auth = new Auth();
-		$auth->set_key(Config::create()->uid)
-			 ->set_password_hash(self::$_user_pass)
-			 ->set_username(self::$_user_name)
+		$auth->set_private_key(self::$_entity_priv)
+			 ->set_public_key(self::$_entity_pub)
 			 ->set_nonce(self::$_api_nonce);
 
 		// create the new nonce
@@ -307,29 +365,40 @@ class Plugin implements PluginInterface
 	}
 
 	/**
-	 * Return if a user is granted to $grantRequested
+	 * Return if a user is granted to $grantRequested or to 'self' grant if a
+	 * user id is provided.
 	 * Possible grants are : read | write | config | delete
 	 *
 	 * @return bool
 	 */
-	public static function is_granted($grantRequested)
+	public static function is_granted($grantRequested, $user_id = 0)
 	{
 		// check the authentification
 		if (!self::is_auth()) return false;
 
-		// retrieve all user's grant
-		try
+		// check virtual 'self' grant if a user id is provided
+		if ($user_id && $user_id == self::_user_id) return true;
+
+		// the entity grants (for better perf on multiple call)
+		static $grants = null;
+
+		if (is_null($grants))
 		{
-			$grants = Model::user_grant_list_by_user(self::$_user_id);
-		}
-		catch(ModelExceptionNoResult $e)
-		{
-			return false;
+			// which entity is logged ?
+			$id = self::$_entity_id ?: Model::user_get_entity_id(self::$_user_id);
+
+			// retrieve all user's grant
+			try
+			{
+				$grants = Model::entity_grant_list_by_entity($id);
+			}
+			catch(ModelExceptionNoResult $e)
+			{
+				return false;
+			}
 		}
 
 		// check if the user is granted to $grantRequested
-		$granted = false;
-
 		foreach($grants as $grant)
 		{
 			if ($grant['name'] == $grantRequested) return true;

app/plugins/auth/WebAuth.php
@@ -39,9 +39,8 @@ class WebAuth
 		$conf = Config::create();
 
 		$auth = new Auth();
-		$auth->set_username($user)
-			 ->set_password_hash($pass)
-			 ->set_key(ADMIN_URL . 'auth-login')
+		$auth->set_public_key($user)
+			 ->set_private_key(md5($pass . ADMIN_URL . 'auth-login'))
 			 ->set_lifetime($conf->plugin_auth->lifetime);
 
 		return $auth;
@@ -89,12 +88,14 @@ class WebAuth
 	 * @param  int    $id
 	 * @param  string $name
 	 * @param  string $pass
+	 * @param  string $email
 	 * @return bool
 	 */
-	protected static function _check_userid($id, &$name, &$pass)
+	protected static function _check_userid($id, &$name, &$pass, &$email = null)
 	{
 		try
 		{
+			// create $name, $pass, $email
 			extract(Model::user_get_by_id($id));
 		}
 		catch (ModelExceptionNoResult $e)
@@ -112,12 +113,14 @@ class WebAuth
 	 * @param  string $user
 	 * @param  int    $id
 	 * @param  string $pass
+	 * @param  string $email
 	 * @return bool
 	 */
-	protected static function _check_username($user, &$id, &$pass)
+	protected static function _check_username($user, &$id, &$pass, &$email = null)
 	{
 		try
 		{
+			// create $name, $pass, $email
 			extract(Model::user_get_by_name($user));
 		}
 		catch (ModelExceptionNoResult $e)
@@ -294,7 +297,7 @@ class WebAuth
 	 */
 	public static function forget(Request $request)
 	{
-		$message = 'an email has been sent to the admin';
+		$message = 'an email has been sent to the account owner';
 
 		try
 		{
@@ -313,11 +316,9 @@ class WebAuth
 			// check if they are not empty
 			if ($user == '') throw new Exception('username is empty');
 
-			// check if user exists (and get it's password)
-			if (self::_check_username($user, $id, $pass))
+			// check if user exists (and get it's password, email)
+			if (self::_check_username($user, $id, $pass, $email))
 			{
-				// send an email to the admin with a reset link
-				$email = Config::create()->email;
 
 				$content = Template::create()
 					->assign('user', $user)
@@ -325,9 +326,14 @@ class WebAuth
 					->render('auth/tpl/forget.php');
 
 				mail($email, 'Pixelpost reset password request', $content);
+
+				// send an email to the admin with a reset link
+				$email = Config::create()->email;
+
+				mail($email, 'Pixelpost reset password request', $content);
 			}
 
-			echo json_encode(array('status' => 'valid', 'message' => 'an email has been sent to the admin'));
+			echo json_encode(array('status' => 'valid', 'message' => $message));
 		}
 		catch(Exception $e)
 		{
@@ -367,7 +373,7 @@ class WebAuth
 			$key = substr($key, 0, 32);
 
 			// check if userID exists
-			if (!self::_check_userid($id, $user, $pass)) throw new Exception();
+			if (!self::_check_userid($id, $user, $pass, $email)) throw new Exception();
 
 			// check if key is valid
 			if ($key != self::_gen_reset_key($id, $user, $pass)) throw new Exception();
@@ -395,7 +401,7 @@ class WebAuth
 					if ($pass == '') throw new Exception('password is empty');
 
 					// change the password in database
-					Model::user_update($id, $user, $pass);
+					Model::user_update($id, $user, $pass, $email);
 
 					self::register($user, $pass, $id, $request->get_host());
 				}

app/plugins/auth/api/auth_config_get.php
@@ -2,9 +2,9 @@
 
 namespace pixelpost\plugins\auth;
 
-use pixelpost;
-use pixelpost\plugins\api\Exception;
+use pixelpost\Config,
+	pixelpost\plugins\api\Exception\Ungranted;
 
-if (!Plugin::is_granted('read')) throw new Exception\Ungranted('auth.config.get');
+if (!Plugin::is_granted('read')) throw new Ungranted('auth.config.get');
 
-$event->response = pixelpost\Config::create()->plugin_auth;
+$event->response = Config::create()->plugin_auth;

commit f273cbaef20c4ad87a703ee860ff55f33ac3f9b5

Plugin Auth: Fix bugs introduced in c84199cf33d3562694a5325eb7d66cfcf5b2018d

app/plugins/auth/Model.php
@@ -402,7 +402,7 @@ class Model
 		Filter::is_int($entity_id);
 
 		$query = 'SELECT name, is_me, user_id, public_key, private_key FROM auth_entity '
-		       . 'WHERE entity_id = %d LIMIT 1;';
+		       . 'WHERE id = %d LIMIT 1;';
 		$query = sprintf($query, $entity_id);
 
 		$db = Db::create();
@@ -726,7 +726,7 @@ class Model
 		// delete old challenge
 		$date = new DateTime();
 
-		$expire = Db::escape(Db::date_serialize());
+		$expire = Db::escape(Db::date_serialize($date));
 
 		$sql = 'DELETE FROM auth_challenge WHERE expire < %s;';
 		$sql = sprintf($sql, $expire);

app/plugins/auth/api/auth_refresh.php
@@ -20,8 +20,8 @@ catch(ModelExceptionNoResult $e)
 	throw new FieldNotValid('token');
 }
 
-// retrieve entity public and private key correspondig to the challenge
-$entity = Model::entity_get_by_id($challenge['entity_id']);
+// retrieve entity public and private key correspondig to the token
+$entity = Model::entity_get_by_id($token['entity_id']);
 
 // retrieve the lifetime configuration value
 $lifetime = Config::create()->plugin_auth->lifetime;