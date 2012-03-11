pixelpost/ultralite2

commit bea7a384163f45e0ca5f9f4a1fc97abb5936a310

Plugin Api: Codec GET: Add urldecode() on extra parameters

app/plugins/api/CodecGet.php
@@ -58,6 +58,9 @@ class CodecGet
 			{
 				list($key, $val) = explode(':', $param, 2);
 
+				$key = Filter::check_encoding(urldecode($key));
+				$val = Filter::check_encoding(urldecode($val));
+
 				$response[$key] = $val;
 			}
 		}

commit 16f7072286735604221c94f341aa3815c5830c00

Plugin Api: Fix Bug: Variable passed by reference...
Bug introduced in bea7a384163f45e0ca5f9f4a1fc97abb5936a310

app/plugins/api/CodecGet.php
@@ -58,8 +58,11 @@ class CodecGet
 			{
 				list($key, $val) = explode(':', $param, 2);
 
-				$key = Filter::check_encoding(urldecode($key));
-				$val = Filter::check_encoding(urldecode($val));
+				$key = urldecode($key);
+				$val = urldecode($val);
+
+				Filter::check_encoding($key);
+				Filter::check_encoding($val);
 
 				$response[$key] = $val;
 			}