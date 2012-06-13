cc8b0a046f7192f4ff66c8310de102eb8d03a053
Alban LEROUX <seza@paradoxal.org>
2012-06-13 16:19:39 +0200

    Plugin Auth: Add plugin configuration in setting page

--- app/plugins/auth/tpl/about.php
+++ app/plugins/auth/tpl/about.php
@@ -1,3 +1,34 @@
+<h3>Configuration</h3>
+
+<form class="well form-inline">
+	<label class="control-label" for="lifetime">Lifetime</label>
+	<input class="input-small" id="lifetime" placeholder="300">
+	<button class="btn btn-primary" type="submit">Update</button>
+</form>
+
+<script class="defered-js" data-defer="defer_plugin_admin_form">
+function defer_plugin_admin_form()
+{
+	var lifetime = $('#lifetime');
+
+	api_call('auth.config.get', {}, function(data) {
+		lifetime.val(data.lifetime);
+	}, function() {
+		notify('error', 'Can\t load auth configuration.', false);
+	});
+
+	$('#lifetime').parent().submit(function(e) {
+		e.preventDefault();
+		e.stopPropagation();
+		api_call('auth.config.set', {'lifetime':lifetime.val()}, function() {
+			notify('success', 'Configuration was updated.');
+		}, function() {
+			notify('error', 'Configuration was not updated.');
+		})
+	});
+}
+</script>
+
 <h3>About</h3>
 <p>
 	The Auth plugin is a piece of Pixelpost base installation, it provides
