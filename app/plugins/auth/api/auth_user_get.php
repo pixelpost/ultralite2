c6a7227fd2f09a2a919289ace7bfa5aecaca2375
Alban LEROUX <seza@paradoxal.org>
2012-06-23 19:27:09 +0200

    Plugin Auth: Add self access to auth.user.get api method

--- app/plugins/auth/api/auth_user_get.php
+++ app/plugins/auth/api/auth_user_get.php
@@ -11,14 +11,21 @@ $method = 'auth.user.get';
 // the request
 $request = $event->request;
 
-// check grants
-if (!Plugin::is_granted('admin')) throw new Ungranted($method);
+// more grant check come later
+if (!Plugin::is_auth()) throw new Ungranted($method);
 
 // input validation
 $user = self::get_required('user', $request, $method);
 
 // check user exists
 if (!self::check_user_name($user, $id, $pass, $mail)) throw new FieldNonExists('user');
+
+// if the user connected request its own data ? (self grant)
+$user_id    = Plugin::get_user_id();
+$self_grant = ($user_id == $id) ? $user_id : 0;
+
+// check grants
+if (!Plugin::is_granted('admin', $self_grant)) throw new Ungranted($method);
 
 // this can feel stupid but it's important to keep a distinction between
 // user identifier and user name, even if today it is the same.
